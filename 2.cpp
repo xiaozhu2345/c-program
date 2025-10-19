136ab2121ff214de4d237683eebecc111dd071bd
xiaozhu2345 <15003593118@163.com>
2025-10-19 13:06:09 +0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "1.h"
+
+int main() {
+    printhello();
+    return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "1.h"
+
+void printhello() {
+    printf("hello world");
+}
